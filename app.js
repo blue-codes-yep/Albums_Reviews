09a2f6ab2341ecb551bcd32c0f0d95eb2eb71af4
Blue Lindsey <linzblue5@gmail.com>
2020-03-06 14:58:52 -0500

    added session stuff

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express'),
+    session = require('express-session'),
+    FileStore = require('session-file-store')(session),
     path = require('path'),
     cookieParser = require('cookie-parser'),
     logger = require('morgan');
-    es6Renderer = require('express-es6-template-engine');
+es6Renderer = require('express-es6-template-engine');
 
 
 const indexRouter = require('./routes/index'),
@@ -20,6 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    store: new FileStore(),
+    secret: 'known',
+    resave: false,
+    saveUninitialized: true,
+    is_logged_in: false
+})
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
